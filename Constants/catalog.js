c6b55081d6636830901bd22435e991ffdd96b8ce
4117 Мисоченко Артем Александрович <super.bananowi4@yandex.ru>
2022-11-06 22:21:04 +0300

    second one

---
+++ Constants/catalog.js
@@ -0,0 +1,44 @@
+const CATALOG = [
+    {
+        id: 'El1',
+        name: 'Квадрацикл LG',
+        img: 'assets/Product1.png',
+        price: 15500,
+    },
+    {
+        id: 'El2',
+        name: 'Лёд для кухни',
+        img: 'assets/Product2.png',
+        price: 1399,
+    },
+    {
+        id: 'El3',
+        name: 'Диван GUCCI',
+        img: 'assets/Product3.png',
+        price: 799,
+    },
+    {
+        id: 'El4',
+        name: 'Велосипед NVIDIA',
+        img: 'assets/Product4.png',
+        price: 400,
+    },
+    {
+        id: 'El5',
+        name: 'Бондана Smart TV',
+        img: 'assets/Product5.png',
+        price: 550,
+    },
+    {
+        id: 'El6',
+        name: 'Прогулка по пустыне',
+        img: 'assets/Product6.png',
+        price: 350,
+    },
+    {
+        id: 'El7',
+        name: 'Пистолет игрушечный детский',
+        img: 'assets/Product7.png',
+        price: 1000,
+    },
+];
---
+++ Header/Header.js
@@ -0,0 +1,32 @@
+class Header {
+    handlerOpenShoppingPage(){
+        shoppingPage.render();
+    }
+    render(){
+        const html = `
+            <div class="header-container">
+                <div id = "sidebar">
+                    <div class = "toggle-btn" onclick="openMenu()">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <ul>
+                        <li>Меню</li>
+                        <li><a href="/">Личный кабинет</a></li>
+                        <li><a onclick="headerPage.handlerOpenShoppingPage();">Корзина</a></li>
+                        <li><a href="/">Главная</a></li>
+                    </ul>     
+                </div>
+                Welcome to Some Shop
+            </div>
+        `;
+
+        ROOT_HEADER.innerHTML= html;
+    }
+}
+function openMenu(){
+    document.getElementById("sidebar").classList.toggle('active');
+}
+const headerPage = new Header;
+headerPage.render();
